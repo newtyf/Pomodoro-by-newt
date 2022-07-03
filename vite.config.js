0f597bc0147ac9b95611426ecefc8a8f0de889c8
Axel Muñoz <axel.munoz@picnic.pe>
2022-07-03 00:04:07 -0500

    config enviroment variables

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    host: false
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [vue(), ],
+    server: {
+      host: env.VITE_HOST
+    }
   }
 })
